ab898032dbb519a3081b6672ad03ba7d5c0864e9
Kaylanne Santos <102864770+kaylannesantos@users.noreply.github.com>
2025-02-09 15:35:40 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:orangescoutfe/view/statScreen.dart';
 import 'view/mainScreen.dart';
 import 'view/historyScreen.dart';
 import 'view/gameScreen.dart';
 import 'view/registerScreen.dart';
 import 'view/loginScreen.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart'; //anuncio
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize(); //Inicializa o SDK do AdMob - Anuncio
+
   runApp(const MyApp());
 }
 
@@ -27,6 +32,7 @@ class MyApp extends StatelessWidget {
         '/game': (context) => GameScreen(),
         '/login': (context) => LoginScreen(),
         '/register': (context) => RegisterScreen(),
+        '/stat': (context) => StatsScreen(),
       },
     );
   }
